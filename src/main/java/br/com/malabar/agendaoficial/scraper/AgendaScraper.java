018c14cbcfe7958f5055e7884b9d9804e5ff9cab
de-victor <12victor.ds@gmail.com>
2023-01-15 01:24:12 -0300

    atualização de lib e correção de codigo

--- src/main/java/br/com/malabar/agendaoficial/scraper/AgendaScraper.java
+++ src/main/java/br/com/malabar/agendaoficial/scraper/AgendaScraper.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -24,13 +25,15 @@ import lombok.extern.java.Log;
 @Component
 public class AgendaScraper {
 
-	private final String url = "https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/agenda-do-presidente-da-republica/";
+	private final String url = "https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/agenda-do-presidente-da-republica-lula/agenda-do-presidente-da-republica/";
 	private final String linha = "item-compromisso";
 	private final String compromissoTitulo = "compromisso-titulo";
 	private final String compromissoLocal = "compromisso-local";
 	private final String horaInicio = "compromisso-inicio";
 	private final String horaFim = "compromisso-fim";
 	private final String viagem = "Partida de";
+
+	private final String embarque = "Embarque";
 	
 	@Autowired
 	private DateUtil dateUtil;
@@ -38,7 +41,7 @@ public class AgendaScraper {
 	
 	
 	public List<Compromisso> scrapAgenda(String data){
-		List<Compromisso> lista = new ArrayList<Compromisso>();
+		List<Compromisso> lista = new ArrayList();
 		
 		try {
 			log.info("Processando URL: "+url+data);
@@ -59,13 +62,12 @@ public class AgendaScraper {
 					compromisso.setUrl(url+data);
 					loadDataToCompromisso(compromisso);
 					
-					if(compromisso.getDescricao().contains(viagem)) {
+					if(checkIfViagem(compromisso.getDescricao())) {
 						compromisso.setTipoCompromisso(TipoCompromissoEnum.Viagem);
 					}
 					else {
 						compromisso.setTipoCompromisso(TipoCompromissoEnum.Compromisso);
 					}
-					
 					
 					log.info("Objeto extraido: "+compromisso);
 					lista.add(compromisso);
@@ -73,41 +75,46 @@ public class AgendaScraper {
 	
 			}
 			
-			
+		} catch (HttpStatusException e){
+			log.info("URL: "+e.getUrl()+" invalida, possívelmente dia sem compromisso");
 		} catch (IOException e) {
 			log.severe("Erro ao processar página de destino");
 			e.printStackTrace();
 		}
-		
+
 		return lista;
 	}
-	
-	public List<Compromisso> scrapAgenda(){
-		List<Compromisso> lista = this.scrapAgenda(this.buildToDayDateUrl());
-		
-		return lista;
+
+	public Boolean checkIfViagem(String line){
+		return line.contains(embarque) || line.contains(viagem);
 	}
 	
+	public List<Compromisso> scrapAgenda() { return this.scrapAgenda(this.buildToDayDateUrl()); }
 	
 	private String buildToDayDateUrl() {
 		LocalDate hoje = LocalDate.now();
-		
+
 		return hoje.getYear()+"-"+
 			   StringUtils.leftPad(hoje.getMonthValue()+"", 2, "0")+"-"+
 		       StringUtils.leftPad(hoje.getDayOfMonth()+"", 2, "0");
 	}
-	
+
+	/*
+	TODO
+	no novo governo a agenda não informa o tempo aproximado de cada compromisso
+	 */
 	private void loadDataToCompromisso(Compromisso compromisso) {
-		LocalDate hoje = LocalDate.now();
-		String[] inicioHoraMinuto = compromisso.getHoraInicio().split("h");
-		String[] fimHoraMinuto = compromisso.getHoraFim().split("h");
+		//LocalDate hoje = LocalDate.now();
+		//String[] inicioHoraMinuto = compromisso.getHoraInicio().split("h");
+		//String[] fimHoraMinuto = compromisso.getHoraFim().split("h");
 		
-		LocalDateTime agora = LocalDateTime.of(hoje.getYear(), hoje.getMonthValue(), hoje.getDayOfMonth(), new Integer(inicioHoraMinuto[0]), new Integer(inicioHoraMinuto[1]));
-		LocalDateTime depois = LocalDateTime.of(hoje.getYear(), hoje.getMonthValue(), hoje.getDayOfMonth(), new Integer(fimHoraMinuto[0]), new Integer(fimHoraMinuto[1]));
+		//LocalDateTime agora = LocalDateTime.of(hoje.getYear(), hoje.getMonthValue(), hoje.getDayOfMonth(), new Integer(inicioHoraMinuto[0]), new Integer(inicioHoraMinuto[1]));
+		//LocalDateTime depois = LocalDateTime.of(hoje.getYear(), hoje.getMonthValue(), hoje.getDayOfMonth(), new Integer(fimHoraMinuto[0]), new Integer(fimHoraMinuto[1]));
 		
-		Duration between = Duration.between(agora, depois);
+		//Duration between = Duration.between(agora, depois);
 		
-		compromisso.setTempoMinutos(between.toMinutes());
+		//compromisso.setTempoMinutos(between.toMinutes());
+		compromisso.setTempoMinutos(0L);
 	}
 	
 	
